e5c312402cbb249e602e6889789bc07f7875e743
TheLuckyMojo <ethanmorales0730@gmail.com>
2021-10-18 20:24:05 -0700

    Inital UI commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import { Button } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class NameForm extends React.Component <any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      targets: 8,
+      attackers: 13
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: any) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event: any) {
+    alert(this.state.targets + ' targets and ' + this.state.attackers + ' attackers');
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Number of targets:
+          <input type="text" value={this.state.targets} onChange={this.handleChange} />
+        </label>
+        <br/>
+        <label>
+          Number of attackers:
+          <input type="text" value={this.state.attackers} onChange={this.handleChange} />
+        </label>
+        <br/>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-export default App;
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById('root')
+);
+
+export default NameForm;
