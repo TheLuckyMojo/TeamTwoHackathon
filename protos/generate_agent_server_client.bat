a008c3c94917bb5c6774a9b96dad8e0c26310cfd
gabby thomas <gabrient@gmail.com>
2021-10-19 23:31:27 -0700

    doy this isn't BAT syntax

--- protos/generate_agent_server_client.bat
+++ protos/generate_agent_server_client.bat
@@ -1,3 +1,3 @@
 python -m grpc_tools.protoc -I./ --python_out=../Backend/agent-server --grpc_python_out=../Backend/agent-server agent.proto
 
-# TODO generate JS client from same proto file into frontend directory
+rem TODO generate JS client from same proto file into frontend directory
