81558627c168edfdfb8806056b944dc00a64ef94
TheLuckyMojo <ethanmorales0730@gmail.com>
2021-10-20 22:26:05 -0700

    Table w/ select

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
-import { Button, TextField } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
 
-class NameForm extends React.Component <any, any> {
+class NameForm extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
@@ -15,18 +15,67 @@ class NameForm extends React.Component <any, any> {
   }
 
   handleChange(event: any) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
   render() {
+    function createData(
+      name: string,
+      status: string,
+    ) {
+      return { name, status };
+    }
+
+    const rows = [
+      createData('Ship 1', 'None'),
+      createData('Ship 2', 'Disabled'),
+      createData('Ship 3', 'Disabled'),
+      createData('Ship 4', 'Destroyed'),
+      createData('Ship 5', 'Destroyed'),
+    ];
     return (
-      <form>
-        <TextField id="outlined-basic" label="Number of targets:" variant="outlined" margin="normal" value={this.state.targets} onChange={this.handleChange}/>
-        <br/>
-        <TextField id="outlined-basic" label="Number of attackers:" variant="outlined" margin="normal" value={this.state.attackers} onChange={this.handleChange}/>
-        <br/>
-        <Button variant="contained" onClick={() => {alert(this.state.targets + ' targets and ' + this.state.attackers + ' attackers')}}>Submit</Button>
-      </form>
+      <div>
+        <form>
+          <TextField id="outlined-basic" label="Number of targets:" variant="outlined" margin="normal" value={this.state.targets} onChange={this.handleChange} />
+          <br />
+          <TextField id="outlined-basic" label="Number of attackers:" variant="outlined" margin="normal" value={this.state.attackers} onChange={this.handleChange} />
+          <br />
+          <Button variant="contained" onClick={() => { alert(this.state.targets + ' targets and ' + this.state.attackers + ' attackers') }}>Sumulate</Button>
+        </form>
+
+        <TableContainer>
+          <Table aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Target</TableCell>
+                <TableCell>Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                >
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">Status</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                      >
+                        <MenuItem>None</MenuItem>
+                        <MenuItem>Disabled</MenuItem>
+                        <MenuItem>Destroyed</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     );
   }
 }
